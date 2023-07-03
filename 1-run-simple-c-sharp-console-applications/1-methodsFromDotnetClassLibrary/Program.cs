a49b33430eae4bc91a44d687b9cfde39821fbc55
George Fincher <george@grimfunky.dev>
2023-07-03 21:03:06 +0100

    Feat 🎯: Complete methodsFromDotnetClassLibrary
    Completed the exercises for the methodsFromDotnetClassLibrary course.

---
+++ 1-run-simple-c-sharp-console-applications/1-methodsFromDotnetClassLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+// int result = Random.Next();
+
+// var number = 7;
+// var text = "seven";
+
+// Console.WriteLine(number);
+// Console.WriteLine();
+// Console.WriteLine(text);
+
+// Random dice = new Random();
+// int roll = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
