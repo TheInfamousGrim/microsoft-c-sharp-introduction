6671c773bd38a5dc570e7a3673940346a747eff1
George Fincher <george@grimfunky.dev>
2023-07-06 20:35:29 +0100

    Feat ✨: Complete challenge activity 3

---
+++ 2-add-logic-to-console-applications/3-challengeActivity/Program.cs
@@ -0,0 +1,33 @@
+﻿// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+
+// Console.WriteLine($"Coin flip: {(flip == 0 ? "heads" : "tails")}");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+Console.WriteLine(permission.Contains("Admin"));
+
+if (permission.Contains("Admin")) {
+    if (level > 55) {
+        Console.WriteLine("Welcome, Super Admin user.");
+    } else {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+} else if (permission.Contains("Manager")) {
+    if (level > 20) {
+        Console.WriteLine("Contact an Admin for access.");
+    } else {
+        Console.WriteLine("You do not have sufficient privileges");
+    }
+} else {
+    Console.WriteLine("You do not have sufficient privileges");
+}
+
+// if (permission.Contains("Admin")) { 
+//     level > 55 ? Console.WriteLine("Welcome, Super Admin user.") : Console.WriteLine("Welcome, Admin user.");
+// } else if (permission.Contains("Manager")) {
+//     level > 20 ? Console.WriteLine("Contact an Admin for access.") : Console.WriteLine("You do not have sufficient privileges");
+// } else {
+//     Console.WriteLine("You do not have sufficient privileges");
+// }
